8bda7de5e18685b08da552096876fa6e3056f4ed
qzj2333 <l.wang12@wustl.edu>
2020-04-13 11:53:35 -0500

    Add files via upload

--- ACE/ACESimpleClient/ACESimpleClient.cpp
+++ ACE/ACESimpleClient/ACESimpleClient.cpp
@@ -2,8 +2,45 @@
 #include "ace/INET_Addr.h"
 #include "ace/SOCK_Stream.h"
 #include "ace/SOCK_Connector.h"
-
+#include "ace/Event_Handler.h"
+#include "ace/Timer_Queue_Adapters.h"
+#include "ace/Timer_Heap.h"
+#include "ace/Reactor.h"
 using namespace std;
+
+class Event: public ACE_Event_Handler
+{
+	int size;
+	char** msg;
+	ACE_INET_Addr* server;
+	ACE_SOCK_Stream stream;
+	ACE_SOCK_Connector connector;
+public:
+	Event(int argc, char* argv[])
+	{
+		size = argc;
+		msg = argv;
+		server = new ACE_INET_Addr(1025, ACE_LOCALHOST);
+	};
+
+	virtual int handle_timeout (const ACE_Time_Value & atv, const void* p=0)
+	{
+		int call = connector.connect(stream, *server);
+		if (call >= 0) 	// call success
+		{
+			for(int i = 1; i < size; i++)
+			{
+				stream.send_n(msg[i], sizeof(msg[i]));
+			}
+			stream.close();
+		}
+		else
+		{
+			cerr << "Fail to connect server" << endl;
+		}
+		return 0;
+	};
+};
 
 int main(int argc, char* argv[])
 {
@@ -14,14 +51,14 @@ int main(int argc, char* argv[])
 	}
 	else
 	{
-		// 2
+		// 1-2
 		/*for(int i = 1; i < argc; i++)
 		{
 			cout << argv[i] << endl;
 		}*/
 		
-		// 5
-		ACE_INET_Addr server(1025, ACE_LOCALHOST);
+		// 1-5
+		/*ACE_INET_Addr server(1025, ACE_LOCALHOST);
 		ACE_SOCK_Stream stream;
 		ACE_SOCK_Connector connector;
 
@@ -38,9 +75,24 @@ int main(int argc, char* argv[])
 		else
 		{
 			cerr << "Fail to connect server" << endl;
-		}
+		}*/
+		
+		// 2-4
+		ACE_Thread_Timer_Queue_Adapter<ACE_Timer_Heap> timer;
+		timer.activate();
+		ACE_Time_Value intervalTime(3, 0);
+		/*ACE_Time_Value startTime = ACE_OS::gettimeofday();
+		startTime += intervalTime;*/
+		ACE_Time_Value delayTime (0,0);
+
+		Event e(argc, argv);
+		//e.handle_timeout(intervalTime); // 2-3
+		
+		/*timer.schedule(&e, 0, startTime, intervalTime);
+		ACE_Thread_Manager::instance()->wait();*/
+		ACE_Reactor::instance()->schedule_timer(&e, 0, delayTime, intervalTime);
+		ACE_Reactor::instance()->run_event_loop();
 		
 		return 0;
 	}
-	
 }
--- ACE/ACESimpleServer/ACESimpleServer.cpp
+++ ACE/ACESimpleServer/ACESimpleServer.cpp
@@ -2,11 +2,59 @@
 #include "ace/INET_Addr.h"
 #include "ace/SOCK_Stream.h"
 #include "ace/SOCK_Acceptor.h"
+#include "ace/Event_Handler.h"
+#include "ace/Reactor.h"
 using namespace std;
+
+// 2-5
+class Event: public ACE_Event_Handler
+{
+	ACE_INET_Addr* server;
+	ACE_SOCK_Stream stream;
+	ACE_SOCK_Acceptor acceptor;
+public:
+	Event()
+	{
+		server = new ACE_INET_Addr(1025, ACE_LOCALHOST);
+		int call = acceptor.open(*server, 1);
+		if(call >= 0)
+		{
+			ACE_TCHAR address[INET6_ADDRSTRLEN];
+			server->addr_to_string(address, sizeof(address));
+			ACE_OS::printf("%s\n", address);
+		}
+	};
+	
+	virtual int handle_input (ACE_HANDLE h = ACE_INVALID_HANDLE)
+	{
+		int connect = acceptor.accept(stream);
+		if(connect>=0)
+		{
+			char msg[BUFSIZ];
+			while(stream.recv(msg, sizeof(msg)) > 0)
+			{
+				cout << msg << endl;
+			}
+			stream.close();
+			cout << "client.leave" << endl;
+			return 0;
+		}
+		else
+		{
+			cerr << "Fail to connect" << endl;
+			return 1;
+		}
+	};
+	
+	virtual ACE_HANDLE get_handle() const
+	{
+		return acceptor.get_handle();
+	};
+};
 
 int main(int argc, char* argv[])
 {
-		if(argc > 1)
+	if(argc > 1)
 	{
 		cerr << "has command line arguments" << endl;
 		return 1;
@@ -17,10 +65,9 @@ int main(int argc, char* argv[])
 		//cout << argv[0] << endl;
 		
 		// 4
-		ACE_INET_Addr server(1025, ACE_LOCALHOST);
+		/*ACE_INET_Addr server(1025, ACE_LOCALHOST);
 		ACE_SOCK_Stream stream;
 		ACE_SOCK_Acceptor acceptor;
-
 		int call = acceptor.open(server, 1);
 		
 		if (call >= 0) 	// call success
@@ -28,21 +75,33 @@ int main(int argc, char* argv[])
 			ACE_TCHAR address[INET6_ADDRSTRLEN];
 			server.addr_to_string(address, sizeof(address));
 			ACE_OS::printf("%s\n", address);
-			int connect = acceptor.accept(stream);
-			if(connect >= 0)
+			int connect = acceptor.accept(e.stream);
+			while(connect >= 0)
 			{
 				char msg[BUFSIZ];
-				while (stream.recv(msg, sizeof(msg)) > 0)
+				while(stream.recv(msg, sizeof(msg)) > 0)
 				{
 					cout << msg << endl;
 				}
 				stream.close();
+				cout << "client.leave" << endl;
+				connect = acceptor.accept(e.stream);
 			}
-			else
-			{
-				cerr << "Fail to connect" << endl;
-			}
-		}
-		return 0;
+		}	
+		else
+		{
+			cerr << "Fail to connect" << endl;
+		}*/
+		// 2-5
+		Event e;
+		/*while(true)
+		{
+			e.handle_input();
+		}*/
+		
+		// 2-6
+		ACE_Reactor::instance()->register_handler(&e, ACE_Event_Handler::ACCEPT_MASK);
+		ACE_Reactor::instance()->run_reactor_event_loop();
 	}
+	return 0;
 }
